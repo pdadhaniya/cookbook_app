f8bf4c257514cd196690633c1bf62d0d7060393b
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-24 16:47:32 -0500

    Replace getter and setter with attr_accessor

--- cookbook.rb
+++ cookbook.rb
@@ -1,42 +1,22 @@
 class Cookbook
+	#replaced getter and setter methods
+	attr_accessor :title
+
 	def initialize(title)
 		@title = title
-	end
-#getter method
-	def title
-		@title
-	end
-#setter method
-	def title=(new_title)
-		@title = new_title
 	end
 end
 
 class Recipe
+	#replaced getter and setter methods
+	attr_accessor :title
+	attr_accessor :ingredients
+	attr_accessor :steps
+
 	def initialize(title, ingredients, steps)
 		@title = title
 		@ingredients = ingredients
 		@steps = steps
-	end
-#getter method
-	def title
-		@title
-	end
-	def ingredients
-		@ingredients
-	end
-	def steps
-		@steps
-	end
-#setter method
-	def title=(new_title)
-		@title = new_title
-	end
-	def ingredients=(new_ingredients)
-		@ingredients = new_ingredients
-	end
-	def steps=(new_steps)
-		@steps = new_steps
 	end
 end
 
