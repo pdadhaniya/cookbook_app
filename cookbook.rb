5f087ce3f0d010573bed8df1ebe95fe47655272a
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-24 16:33:25 -0500

    Add setter methods to cookbook and recipe

--- cookbook.rb
+++ cookbook.rb
@@ -6,6 +6,10 @@ class Cookbook
 	def title
 		@title
 	end
+#setter method
+	def title=(new_title)
+		@title = new_title
+	end
 end
 
 class Recipe
@@ -23,6 +27,16 @@ class Recipe
 	end
 	def steps
 		@steps
+	end
+#setter method
+	def title=(new_title)
+		@title = new_title
+	end
+	def ingredients=(new_ingredients)
+		@ingredients = new_ingredients
+	end
+	def steps=(new_steps)
+		@steps = new_steps
 	end
 end
 
