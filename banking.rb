8fea4e892bf1809a26d95fd5b6430dd221f21a43
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-29 10:23:46 -0500

    add person and bank class and initialize method for person with name and cash

---
+++ banking.rb
@@ -0,0 +1,21 @@
+
+
+class Person #made the Person class
+
+	attr_reader :cash_on_hand
+
+	def initialize(name, cash_on_hand=0)
+		@name = name
+		@cash_on_hand = cash_on_hand
+	end
+
+end
+
+
+
+
+
+class Bank #made the Bank class
+
+
+end
